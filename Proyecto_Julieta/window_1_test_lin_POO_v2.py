971ea862211050510ccab1e5d3d6bcf0bc15c82b
Samuel Ibarra <ing.samuelibarra@gmail.com>
2021-10-24 04:01:50 -0500

    cambios importantes

--- Proyecto_Julieta/window_1_test_lin_POO_v2.py
+++ Proyecto_Julieta/window_1_test_lin_POO_v2.py
@@ -1,14 +1,16 @@
 from os import name, path
 from tkinter import *
 from tkinter import Tk, Label, Button, Entry, Frame
+from tkinter import ttk
+import tkinter
 from tkinter.ttk import *
+from typing import Generator
 from pygame import mixer
 import random
 from pathlib import Path
 from PIL import ImageTk
 
 mixer.init()
-
 root = Tk()
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
@@ -25,165 +27,141 @@ MAIN_ROOT = Path(__file__).parent.absolute()
 background = ImageTk.PhotoImage(file=(MAIN_ROOT / "fondo.png").__str__())
 
 
-# despues veo si hago otra cosa xd si ialgo asi para no tener que tenerlo separado.. algo como esto
-# son gaas de jodr
-
-
 class SoundOption:
-    def __init__(self, filename: str, frecuency: int = 60, main_root: Path = MAIN_ROOT) -> None:
-        self.name = filename
+    def __init__(self, name: str, frecuency: int = 60, main_root: Path = MAIN_ROOT) -> None:
+        self.name = name
         self.frecuency = frecuency
+        self.volume = db_levels[0]
 
         self.sound = mixer.Sound((main_root / f"{self.name}.wav").__str__())
         self.image = PhotoImage(
             file=(main_root / f"{self.name}.png").__str__())
+    
+    def play(self):
+        # Cambiar para cada frecuencia...
+        self.sound.set_volume(self.volume)
+        self.sound.play()
+    
+    def add_volume(self):
+        if not self.volume >= db_levels[-1]:
+            self.volume += db_steps
 
 
 class Alert:
-    def __init__(self, filename: str, main_root: Path = MAIN_ROOT) -> None:
-        self.name = filename
+    def __init__(self, name: str, main_root: Path = MAIN_ROOT) -> None:
+        self.name = name
         self.sound = mixer.Sound((main_root / f"{self.name}.wav").__str__())
 
+# sonidos y su frecuencia
+sound_options_dict = {
+    "a": 20,
+    "m": 30,
+    "u": 40,
+    "s": 50,
+    "sh": 60,
+    "i": 70
+}
 
-sound_options_list = [
-    SoundOption('a'),
-    SoundOption('m'),
-    SoundOption('u'),
-    SoundOption('s'),
-    SoundOption('sh'),
-    SoundOption('i'),
-]
+# alertas para el programa
+alert_options_names = ["error", "applause-1"]
 
-alerts_list = [
-    SoundOption('error'),
-    SoundOption('applause-1'),
-]
+sound_options_list = [SoundOption(name, frec) for name, frec in sound_options_dict.items()]
+alerts_dict = {name: Alert(name) for name in alert_options_names}
 
-sounds_list = [obj.sound for obj in sound_options_list]
-
-
+db_steps = 0.2
+db_levels = [db/10 for db in range(db_steps * 10, 11, db_steps * 10)]
+results_dict = {sound_option.name: {db: 0 for db in db_levels} for sound_option in sound_options_list}
 
 class window1(Frame):
 
     def __init__(self, master) -> None:
         super().__init__(master, width=1152, height=1152)
+        
+        # variables
+        self.random_program_selection = None
+        self.start_button_name = StringVar(value="Iniciar Pruebas")
+        self.last_election_memory = None
+        
         self.pack()
+        # Generador de posiciones de figuras. Por ahora solo acepta 9 figuras pero las ultimas 3 medio mal por el ultimo boton de salida que debe correrse
+        self.place_generator = ({"x": x * 338 + 138,
+                                 "y": y * 260 + 120,
+                                 "width": 200,
+                                 "height": 210
+                                 } for y in range(3) for x in range(3))
         self.create_widgets()
         self.canvas1 = Canvas(master, width=1152, height=1152).create_image(
             0, 0, image=background, anchor="nw")
 
+
     def create_widgets(self) -> None:
+        # botones creados con imagenes
+        for sound_option in sound_options_list:
+            self.__setattr__(f"{sound_option.name}_photo", sound_option.image)
+            self.__setattr__(f"{sound_option.name}_image", self.__getattribute__(f"{sound_option.name}_photo").subsample(3,3))
+            self.__setattr__(f"{sound_option.name}_button", Button(self,
+                                   image=self.__getattribute__(f"{sound_option.name}_image"),
+                                   command=self.option_pressed(sound_option)))
+            self.__getattribute__(f"{sound_option.name}_button").place(**self.place_generator.__next__())
 
-        self.photo = [
-            obj.image for obj in sound_options_list if obj.name == 'a'][0]
-        self.photoimage = self.photo.subsample(3, 3)
-        self.button = Button(self, image=self.photoimage, command=self.selecciona).place(
-            x=138, y=120, width=200, height=210)
+        # boton principal
+        self.buttonst = Button(self, textvariable=self.start_button_name, command=self.random_sound_selection, state=NORMAL)
+        self.buttonst.place(x=476, y=40, width=200, height=50)
+        
+        # boton de salida
+        self.buttonexit = Button(self, text="salir", command=root.destroy)
+        self.buttonexit.place(x=476, y=640, width=200, height=50)
 
-        self.photo2 = [
-            obj.image for obj in sound_options_list if obj.name == 'i'][0]
-        self.photoimage2 = self.photo2.subsample(3, 3)
-        self.button2 = Button(self, image=self.photoimage2, command=self.seleccioni).place(
-            x=476, y=120, width=200, height=210)
+    def random_sound_selection(self):
+        if len(sound_options_list) == 0:
+            # en caso de el usuario haber acertado todas las figuras
+            print("FINALIZAMOS CON TODAS LAS FRECUENCIAS")
+            print(results_dict)
+            root.destroy()
+            return
+        
+        # Seleccionamos uno
+        self.random_program_selection = random.choice([sound_option for sound_option in sound_options_list if sound_option != self.last_election_memory])
+        # lo memorizamos para no repetirlo
+        self.last_election_memory = self.random_program_selection
+        print("random choice:", self.random_program_selection.name)
+        
+        # Reproducimos
+        self.random_program_selection.play()
+        #cambiamos nombre a boton
+        self.start_button_name.set("Esperando respuesta...")
+        # desactivamos el boton
+        self.buttonst["state"] = DISABLED
 
-        self.photo3 = [
-            obj.image for obj in sound_options_list if obj.name == 'm'][0]
-        self.photoimage3 = self.photo3.subsample(3, 3)
-        self.button3 = Button(self, image=self.photoimage3, command=self.seleccionm).place(
-            x=864, y=120, width=200, height=210)
+    def option_pressed(self, selection: SoundOption):
+        def check_election():
+            if selection == self.random_program_selection:
+                # reproducimos
+                print("pressed:", selection.name)
+                selection.play()
+                alerts_dict["applause-1"].sound.play()
+                
+                # Guardo el resultado en mi diccionario
+                results_dict[selection.name][selection.volume] += 1
+                # Elimino la opcion que ya fue correcta de las posibles opciones a realizar
+                sound_options_list.remove(selection)
 
-        self.photo4 = [
-            obj.image for obj in sound_options_list if obj.name == 'u'][0]
-        self.photoimage4 = self.photo4.subsample(3, 3)
-        self.button4 = Button(self, image=self.photoimage4, command=self.seleccionu).place(
-            x=138, y=380, width=200, height=210)
+            else:
+                alerts_dict["error"].sound.play()
+                # si falla el programa sube progresivamente el sonido de la figura fallada de 0.2 en 0.2
+                selection.add_volume()
 
-        self.photo5 = [
-            obj.image for obj in sound_options_list if obj.name == 's'][0]
-        self.photoimage5 = self.photo5.subsample(3, 3)
-        self.button5 = Button(self, image=self.photoimage5, command=self.seleccions).place(
-            x=476, y=380, width=200, height=210)
-
-        self.photo6 = [
-            obj.image for obj in sound_options_list if obj.name == 'sh'][0]
-        self.photoimage6 = self.photo6.subsample(3, 3)
-        self.button6 = Button(self, image=self.photoimage6, command=self.seleccionsh).place(
-            x=864, y=380, width=200, height=210)
-
-        self.buttonst = Button(self, text="Reproduce Sonido", command=self.soundeleccion).place(
-            x=476, y=40, width=200, height=50)
-        self.buttonexit = Button(self, text="salir", command=root.destroy).place(
-            x=476, y=640, width=200, height=50)
-
-    def soundeleccion(self):
-        self.soundseleccion = random.choice(
-            [obj.name for obj in sound_options_list])
-        print("el sonido seleccionado")
-        print(self.soundseleccion)
-        self.seleccionsonido()
-        #self.buttonst['command']= self.buttonst.destroy()
-
-        # que no se repita el mismo sonido en dos elecciones de random (Puedes filtrar basado en la ultima eleccion que tomaste)
-        # Guarda la cantidad de aciertos y errores por frecuencia y por imagen. (Puede ser un diccionario. Hay que colocar frecuencia al objeto)
-        # Hay que variar la frecuencia y los decibelios con lo que esta frecuencia se escucha. De menor a mayor con un rango definido. la freucnecia puede ser random
-        # Cambiar el boton de iniciar test por el boton de siguiente sonido una vez iniciado el test.
-        # self.buttons['command'] = self.buttons.destroy() ??
-
-    def selecciona(self) -> None:
-        print("Avion fue presionado")
-        self.seleccion = "a"
-        print(self.seleccion)
-        print(self.soundseleccion)
-        self.eleccion()
-
-    def seleccionm(self) -> None:
-        print("Helado fue presionado")
-        self.seleccion = "m"
-        print(self.seleccion)
-        print(self.soundseleccion)
-        self.eleccion()
-
-    def seleccionu(self) -> None:
-        print("Buho fue presionado")
-        self.seleccion = "u"
-        print(self.seleccion)
-        print(self.soundseleccion)
-        self.eleccion()
-
-    def seleccions(self) -> None:
-        print("Serpiente fue presionado")
-        self.seleccion = "s"
-        print(self.seleccion)
-        print(self.soundseleccion)
-        self.eleccion()
-
-    def seleccionsh(self) -> None:
-        print("Silencio fue presionado")
-        self.seleccion = "sh"
-        print(self.seleccion)
-        print(self.soundseleccion)
-        self.eleccion()
-
-    def seleccioni(self) -> None:
-        print("El raton fue presionado")
-        self.seleccion = "i"
-        print(self.seleccion)
-        print(self.soundseleccion)
-        self.eleccion()
-
-    def eleccion(self):
-
-        if len([obj.sound.play() for obj in sound_options_list if obj.name == self.seleccion and obj.name == self.soundseleccion]) == 0:
-            [obj.sound.play() for obj in sound_options_list if obj.name == 'error']
-
-        if len([obj.sound.play() for obj in sound_options_list if obj.name == self.seleccion and obj.name == self.soundseleccion]) == 1:
-            [obj.sound.play()
-             for obj in sound_options_list if obj.name == 'applause-1']
-
-    def seleccionsonido(self):
-        [obj.sound.play()
-         for obj in sound_options_list if obj.name == self.soundseleccion]
+            # dejamos al programa sin eleccion
+            self.random_program_selection = None
+            # cambiamos nombre a boton principal
+            self.start_button_name.set("Siguiente Sonido")
+            # habilitamos el boton
+            self.buttonst["state"] = NORMAL
+            
+        return check_election
 
 
-app = window1(root)
-app.mainloop()
+
+if __name__ == "__main__":
+    app = window1(root)
+    app.mainloop()
